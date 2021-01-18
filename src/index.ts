321769e9d70e1294d05abf3e781c45b4f767389b
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-01-18 21:46:27 +0200

    Make index.ts follow .eslintrc specs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -7,5 +7,5 @@ const app = express();
 app.use(express.json());
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log("Server started, listening on " + process.env.SERVER_PORT);
+    console.log(`Server started, listening on ${process.env.SERVER_PORT}`);
 });
