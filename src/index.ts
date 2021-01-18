6a75bd49e9576d36a1224a15a58c901a04c2c3a5
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-01-18 19:05:00 +0200

    Add system http application entry point

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Server started, listening on " + process.env.SERVER_PORT);
+});
