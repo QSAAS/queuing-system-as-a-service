09f974b546e9f8dfcf741edc349b02a17b016bdf
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-19 15:07:19 +0200

    fix eslint errors

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-const port  = process.env.SERVER_PORT || 8080 ; 
-app.listen(port , () => {
+const port = process.env.SERVER_PORT || 8080;
+app.listen(port, () => {
     console.log(`Server started, listening on ${port}`);
 });
