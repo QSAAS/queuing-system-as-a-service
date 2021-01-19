423d9e10d841374e92438ce647407dc9b9821671
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-01-19 20:06:55 +0200

    fix dburl undefined

--- src/Reservation/Infrastructure/Persistence/db.ts
+++ src/Reservation/Infrastructure/Persistence/db.ts
@@ -1,11 +1,11 @@
 import  mongoose from 'mongoose'
-let { DB_URL: dbUrl } = process.env ;
+let dbUrl: string | undefined= process.env.DB_URL ;
 /**
  *  connect is async, we have to use it with async and await or provide callback function in
  */
 export default function() {
     if(dbUrl){
-        mongoose.connect(dbUrl)
+        mongoose.connect(dbUrl,{ useUnifiedTopology: true , useNewUrlParser: true})
             .then(() => {
                 console.log(`connected to ${dbUrl} successfully`);
             }).catch((e)=>{
