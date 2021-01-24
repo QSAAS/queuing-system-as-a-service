9937b6ca50973ed5f5e8b9af58f99d7aa70a43b4
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-01-24 20:43:04 +0200

    Added connection to mongodb and handled dotenv reading failure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import clientRouter from "./Client/routes";
 
 dotenv.config();
 
+const serverPort: string | undefined = process.env.SERVER_PORT;
+if (!serverPort) {
+    console.log("dotenv could not read SERVER_PORT");
+    process.exit(1);
+}
+
+const dbURL: string | undefined = process.env.DB_URL;
+if (!dbURL) {
+    console.log("dotenv could not read DB_URL");
+    process.exit(1);
+}
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((reason) => console.log(reason));
+
 const app = express();
+
 app.use(express.json());
-const port = process.env.SERVER_PORT; // TODO handle errors if env fails to read SERVER_PORT
-app.listen(port, () => {
-    console.log(`Server started, listening on ${port}`);
+app.use("/api/clients", clientRouter);
+
+app.listen(serverPort, () => {
+    console.log(`Server started, listening on ${serverPort}`);
 });
