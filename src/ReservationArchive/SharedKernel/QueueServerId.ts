ce252fd117f793e3002fef1867afc58fb87f1072
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-02-04 11:07:20 +0200

    Fixed QueueServerId class name.

--- src/ReservationArchive/SharedKernel/QueueServerId.ts
+++ src/ReservationArchive/SharedKernel/QueueServerId.ts
@@ -1,18 +1,18 @@
 import { v4 as uuidv4 } from "uuid";
 
-export default class OrganizationAccountId {
+export default class QueueServerId {
     private readonly id: string;
 
     constructor(id: string) {
         this.id = id;
     }
 
-    static from(id: string): OrganizationAccountId {
-        return new OrganizationAccountId(id);
+    static from(id: string): QueueServerId {
+        return new QueueServerId(id);
     }
 
-    static create(): OrganizationAccountId {
-        return new OrganizationAccountId(uuidv4());
+    static create(): QueueServerId {
+        return new QueueServerId(uuidv4());
     }
 
     getString(): string {
