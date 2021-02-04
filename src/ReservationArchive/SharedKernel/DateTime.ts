f1546b3b4e2210f22075b85e6e8a79087124d02f
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-02-04 10:59:55 +0200

    Added DateTime value object using typescript's builtin date for now.

---
+++ src/ReservationArchive/SharedKernel/DateTime.ts
@@ -0,0 +1,17 @@
+export default class DateTime {
+    // TODO: Make DateTime interface since it can have multiple implementations
+    // This is a placeholder implementation using typescript's date time until we decide on a library or something.
+    private readonly dateTime: Date;
+
+    constructor(dateTime: Date) {
+        this.dateTime = dateTime;
+    }
+
+    public from(dateTime: string): DateTime {
+        return new DateTime(new Date(dateTime));
+    }
+
+    public getDateTimeString(): string {
+        return this.dateTime.toDateString();
+    }
+}
