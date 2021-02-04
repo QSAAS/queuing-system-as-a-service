4ee05655003cfedf4a41d64628355f9c5ad717fe
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-04 17:43:28 +0200

    Moved MongoClientModel.ts into MongoClientRepository.ts

--- src/Client/Infrastructure/MongoClientRepository.ts
+++ src/Client/Infrastructure/MongoClientRepository.ts
@@ -1,17 +1,65 @@
-import { Document } from "mongoose";
+import {
+    model, Document, Schema, Model,
+} from "mongoose";
+import ClientId from "@app/Client/Domain/ValueObjects/ClientId";
 import ClientRepository from "../Domain/Repositories/ClientRepository";
 import Client from "../Domain/Entities/Client";
-import MongoClientModel from "./MongoClientModel";
+
+/**
+ * FixMe discuss: this interface does not serve the purpose we intend it to,
+ * I think the only thing it does is provide type hints for mongoose model instances.
+ * However, it does not provide any error checking whatsoever.
+ */
+interface IClient extends Document {
+    id: string;
+    username: string;
+    password: string;
+    email: string;
+}
+
+const schema: Schema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 50,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        maxlength: 256,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 5,
+        maxlength: 255,
+    },
+});
+
+const mongooseClient: Model<IClient> = model("Client", schema);
 
 class MongoClientRepository implements ClientRepository {
-    create(client: Client): Promise<Document> {
-        const mongoClient = new MongoClientModel({
+    async save(client: Client): Promise<Client> {
+        const mongoClient = await mongooseClient.create({
             id: client.getId().toString(),
             username: client.getUsername(),
             password: client.getPassword(),
             email: client.getEmail(),
         });
-        return mongoClient.save();
+        return MongoClientRepository.toClient(mongoClient);
+    }
+
+    private static toClient(mongoClient: IClient): Client {
+        return new Client(ClientId.from(mongoClient.id), mongoClient.username, mongoClient.password, mongoClient.email);
     }
 }
 
