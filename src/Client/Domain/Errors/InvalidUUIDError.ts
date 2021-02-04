2e27a5716fdc509aa328047828d1e191ee7e258f
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-04 17:42:14 +0200

    Added error checking on ClientId.from()

---
+++ src/Client/Domain/Errors/InvalidUUIDError.ts
@@ -0,0 +1,3 @@
+export default class InvalidUUIDError extends Error {
+
+}
--- src/Client/Domain/ValueObjects/ClientId.ts
+++ src/Client/Domain/ValueObjects/ClientId.ts
@@ -1,9 +1,13 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4, validate as isValidUUID } from "uuid";
+import InvalidUUIDError from "@app/Client/Domain/Errors/InvalidUUIDError";
 
 class ClientId {
     constructor(private readonly id: string) {}
 
-    static from(id: string): ClientId { // What the hell is this supposed to do?
+    static from(id: string): ClientId | never {
+        if (!isValidUUID(id)) {
+            throw new InvalidUUIDError(`'${id}' is not a valid UUID`);
+        }
         return new ClientId(id);
     }
 
