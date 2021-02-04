9407be6ebbc27dfa0d6520755a44d7bea980d773
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-04 17:48:50 +0200

    Changed RegisterService.run() to return Promise<DTO>

--- src/Client/Application/Services/RegisterService.ts
+++ src/Client/Application/Services/RegisterService.ts
@@ -1,13 +1,16 @@
 import ClientRepository from "@app/Client/Domain/Repositories/ClientRepository";
 import RegisterDTO from "@app/Client/Application/DataTransferObjects/RegisterDTO";
 import ClientFactory from "@app/Client/Domain/Factories/ClientFactory";
+import RegisterResponseDTO from "@app/Client/Application/DataTransferObjects/RegisterResponseDTO";
+import Client from "@app/Client/Domain/Entities/Client";
 
 class RegisterService {
     constructor(private repository: ClientRepository, private factory: ClientFactory) {}
 
-    public run(requestDTO: RegisterDTO) {
-        const client = this.factory.create(requestDTO.username, requestDTO.password, requestDTO.email);
-        return this.repository.create(client);
+    public async run(requestDTO: RegisterDTO): Promise<RegisterResponseDTO> {
+        const client = await this.factory.create(requestDTO.username, requestDTO.password, requestDTO.email);
+        const registerResponse: Client = await this.repository.save(client);
+        return RegisterResponseDTO.from(registerResponse);
     }
 }
 
