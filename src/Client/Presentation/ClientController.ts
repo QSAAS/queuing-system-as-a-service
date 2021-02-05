541fcccbb966950466de3e631e0708c08972ae33
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-05 17:42:52 +0200

    Added login function to ClientController

--- src/Client/Presentation/ClientController.ts
+++ src/Client/Presentation/ClientController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import RegisterDTO from "@app/Client/Application/DataTransferObjects/RegisterDTO";
 import RegisterResponseDTO from "@app/Client/Application/DataTransferObjects/RegisterResponseDTO";
+import LoginService from "@app/Client/Application/Services/LoginService";
+import LoginDTO from "@app/Client/Application/DataTransferObjects/LoginDTO";
+import LoginResponseDTO from "@app/Client/Application/DataTransferObjects/LoginResponseDTO";
 import RegisterService from "../Application/Services/RegisterService";
 
 class ClientController {
@@ -12,6 +15,16 @@ class ClientController {
         );
         try {
             const responseDTO: RegisterResponseDTO = await registerService.run(dto);
+            response.status(200).send(responseDTO);
+        } catch (e) {
+            response.status(400).send(e.message);
+        }
+    }
+
+    public async login(request: Request, response: Response, loginService: LoginService): Promise<void> {
+        const dto = new LoginDTO(request.body.username, request.body.password);
+        try {
+            const responseDTO: LoginResponseDTO = await loginService.run(dto);
             response.status(200).send(responseDTO);
         } catch (e) {
             response.status(400).send(e.message);
