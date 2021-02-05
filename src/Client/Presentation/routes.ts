6ae9781aa0d495c68389ac5bee2dc866702a86d5
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-05 17:43:43 +0200

    Added login route to routes.ts

--- src/Client/Presentation/routes.ts
+++ src/Client/Presentation/routes.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import MongoClientRepository from "@app/Client/Infrastructure/MongoClientRepository";
 import RegisterService from "@app/Client/Application/Services/RegisterService";
 import ClientFactory from "@app/Client/Domain/Factories/ClientFactory";
+import LoginService from "@app/Client/Application/Services/LoginService";
 import ClientController from "./ClientController";
 
 const router = Router();
@@ -10,6 +11,12 @@ router.post("/", async (request: Request, response: Response) => {
     const registerService = new RegisterService(new MongoClientRepository(), new ClientFactory());
     const clientController = new ClientController();
     await clientController.register(request, response, registerService);
+});
+
+router.post("/login", async (request: Request, response: Response) => {
+    const loginService: LoginService = new LoginService(new MongoClientRepository());
+    const clientController: ClientController = new ClientController();
+    await clientController.login(request, response, loginService);
 });
 
 export default router;
