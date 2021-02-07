ee93d3f2182cb510e44c9ebb56ff1dccdb4963f6
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-02-07 13:47:08 +0200

    Added getters to FinishedReservation Entity.

--- src/ReservationArchive/Domain/Entity/FinishedReservation.ts
+++ src/ReservationArchive/Domain/Entity/FinishedReservation.ts
@@ -1,9 +1,9 @@
-import ReservationId from "@app/ReservationArchive/SharedKernel/ReservationId";
-import ClientId from "@app/ReservationArchive/SharedKernel/ClientId";
-import QueuingNodeId from "@app/ReservationArchive/SharedKernel/QueuingNodeId";
-import DateTime from "@app/ReservationArchive/SharedKernel/DateTime";
-import QueueServerId from "@app/ReservationArchive/SharedKernel/QueueServerId";
-import OrganizationAccountId from "@app/ReservationArchive/SharedKernel/OrganizationAccountId";
+import ReservationId from "@app/SharedKernel/ValueObject/ReservationId";
+import ClientId from "@app/SharedKernel/ValueObject/ClientId";
+import QueuingNodeId from "@app/SharedKernel/ValueObject/QueuingNodeId";
+import DateTime from "@app/SharedKernel/ValueObject/DateTime";
+import QueueServerId from "@app/SharedKernel/ValueObject/QueueServerId";
+import OrganizationAccountId from "@app/SharedKernel/ValueObject/OrganizationAccountId";
 
 export default class FinishedReservation {
     private readonly reservationId: ReservationId;
@@ -40,5 +40,37 @@ export default class FinishedReservation {
                        queueServerId: QueueServerId, serverOperatorId: OrganizationAccountId): FinishedReservation {
         return new FinishedReservation(reservationId, clientId, queuingNodeId, reservationTime, servingStartTime,
                                        servingFinishTime, queueServerId, serverOperatorId);
+    }
+
+    getReservationId(): ReservationId {
+        return this.reservationId;
+    }
+
+    getClientId(): ClientId {
+        return this.clientId;
+    }
+
+    getQueuingNodeId(): QueuingNodeId {
+        return this.queuingNodeId;
+    }
+
+    getReservationTime(): DateTime {
+        return this.reservationTime;
+    }
+
+    getServingStartTime(): DateTime {
+        return this.servingStartTime;
+    }
+
+    getServingFinishTime(): DateTime {
+        return this.servingFinishTime;
+    }
+
+    getQueueServerId(): QueueServerId {
+        return this.queueServerId;
+    }
+
+    getServerOperatorId(): OrganizationAccountId {
+        return this.serverOperatorId;
     }
 }
