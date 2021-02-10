2dc974ba61808f194bad84d5f694ffbc67b067cb
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-11 01:18:45 +0200

    Refactoring imports after renaming repository interface

--- src/ReservationArchive/Infrastructure/Persistence/Mongoose/Repository/CancelledReservationRepository.ts
+++ src/ReservationArchive/Infrastructure/Persistence/Mongoose/Repository/CancelledReservationRepository.ts
@@ -1,28 +1,29 @@
-import ICancelledReservationRepository from "@app/ReservationArchive/Domain/Service/CancelledReservationRepository";
+import ICancelledReservationRepository from "@app/ReservationArchive/Domain/Service/ICancelledReservationRepository";
 import ClientId from "@app/SharedKernel/ValueObject/ClientId";
 import CancelledReservation from "@app/ReservationArchive/Domain/Entity/CancelledReservation";
 import PersistenceError from "@app/ReservationArchive/Domain/Error/Persistence/PersistenceError";
 import {
     CancelledReservationModel,
-    CancelledReservationDoc
+    CancelledReservationDoc,
 } from "@app/ReservationArchive/Infrastructure/Persistence/Mongoose/Model/CancelledReservationModel";
 
 export default class CancelledReservationRepository implements ICancelledReservationRepository {
     private readonly CancelledReservationModel: CancelledReservationModel;
 
-    constructor(CancelledReservationModel: CancelledReservationModel) {
-        this.CancelledReservationModel = CancelledReservationModel;
+    constructor(cancelledReservationModel: CancelledReservationModel) {
+        this.CancelledReservationModel = cancelledReservationModel;
     }
 
     public async getClientReservations(clientId: ClientId): Promise<CancelledReservation[]> {
         try {
             const cancelledReservationDocs: CancelledReservationDoc[] = await this.CancelledReservationModel.find(
-                    { clientId: clientId.toString() },
+                { clientId: clientId.toString() },
             );
             const cancelledReservations: CancelledReservation[] = [];
             for (let i = 0; i < cancelledReservationDocs.length; ++i) {
                 cancelledReservations.push(
-                        this.CancelledReservationModel.toCancelledReservationEntity(cancelledReservationDocs[i]));
+                    this.CancelledReservationModel.toCancelledReservationEntity(cancelledReservationDocs[i]),
+                );
             }
             return cancelledReservations;
         } catch (e) {
@@ -31,8 +32,9 @@ export default class CancelledReservationRepository implements ICancelledReserva
     }
 
     public async save(cancelledReservation: CancelledReservation): Promise<void> {
-        const cancelledReservationDoc: CancelledReservationDoc = this.CancelledReservationModel.toCancelledReservationDoc(
-                cancelledReservation);
+        const cancelledReservationDoc = this.CancelledReservationModel.toCancelledReservationDoc(
+            cancelledReservation,
+        );
         try {
             await cancelledReservationDoc.save();
         } catch (e) {
