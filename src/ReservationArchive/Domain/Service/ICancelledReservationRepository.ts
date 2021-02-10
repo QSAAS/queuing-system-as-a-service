f2876a8baa3be56c98d16ad6cfe40f74531b2008
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-11 01:15:18 +0200

    Renamed repository interfaces

--- src/ReservationArchive/Domain/Service/ICancelledReservationRepository.ts
+++ src/ReservationArchive/Domain/Service/ICancelledReservationRepository.ts
@@ -1,7 +1,7 @@
 import ClientId from "@app/SharedKernel/ValueObject/ClientId";
 import CancelledReservation from "@app/ReservationArchive/Domain/Entity/CancelledReservation";
 
-export default interface CancelledReservationRepository {
+export default interface ICancelledReservationRepository {
     getClientReservations(clientId: ClientId): Promise<CancelledReservation[]>;
 
     save(cancelledReservation: CancelledReservation): Promise<void>;
--- src/ReservationArchive/Domain/Service/IFinishedReservationRepository.ts
+++ src/ReservationArchive/Domain/Service/IFinishedReservationRepository.ts
@@ -1,7 +1,7 @@
 import ClientId from "@app/SharedKernel/ValueObject/ClientId";
 import FinishedReservation from "@app/ReservationArchive/Domain/Entity/FinishedReservation";
 
-export default interface FinishedReservationRepository {
+export default interface IFinishedReservationRepository {
     getClientReservations(clientId: ClientId): Promise<FinishedReservation[]>
 
     save(finishedReservation: FinishedReservation): Promise<void>
