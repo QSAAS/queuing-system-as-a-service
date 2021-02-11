2715b6cdc69a16280c072d48c77c4856a20e328e
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-11 11:13:35 +0200

    Created GetCancelledReservationsService

---
+++ src/ReservationArchive/Application/Services/GetCancelledReservationsService.ts
@@ -0,0 +1,23 @@
+import ICancelledReservationRepository from "@app/ReservationArchive/Domain/Service/ICancelledReservationRepository";
+import ClientIdDTO from "@app/ReservationArchive/Application/DataTransferObjects/ClientIdDTO";
+import ClientId from "@app/SharedKernel/ValueObject/ClientId";
+import CancelledReservationDTO from "@app/ReservationArchive/Application/DataTransferObjects/CancelledReservationDTO";
+import CancelledReservation from "@app/ReservationArchive/Domain/Entity/CancelledReservation";
+import CancelledReservationTransformer
+    from "@app/ReservationArchive/Application/Transformers/CancelledReservationTransformer";
+
+export default class GetCancelledReservationsService {
+    constructor(private repository: ICancelledReservationRepository) {}
+
+    public async run(requestDTO: ClientIdDTO): Promise<CancelledReservationDTO[]> {
+        const clientId: ClientId = ClientId.from(requestDTO.clientId);
+        const cancelledReservations: CancelledReservation[] = await this.repository.getClientReservations(clientId);
+        const cancelledReservationsDTO: CancelledReservationDTO[] = [];
+        for (let i = 0; i < cancelledReservations.length; ++i) {
+            cancelledReservationsDTO.push(
+                CancelledReservationTransformer.toDTO(cancelledReservations[i]),
+            );
+        }
+        return cancelledReservationsDTO;
+    }
+}
