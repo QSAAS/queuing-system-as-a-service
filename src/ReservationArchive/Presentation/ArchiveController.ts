8417b9402bf18be9ac313efafdeb314ebd8ef71a
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-11 11:14:11 +0200

    Added function getCancelledReservations to ArchiveController

--- src/ReservationArchive/Presentation/ArchiveController.ts
+++ src/ReservationArchive/Presentation/ArchiveController.ts
@@ -5,6 +5,9 @@ import ArchiveCancelledReservationService
 import ArchiveFinishedReservationService
     from "@app/ReservationArchive/Application/Services/ArchiveFinishedReservationService";
 import FinishedReservationDTO from "@app/ReservationArchive/Application/DataTransferObjects/FinishedReservationDTO";
+import GetCancelledReservationsService
+    from "@app/ReservationArchive/Application/Services/GetCancelledReservationsService";
+import ClientIdDTO from "@app/ReservationArchive/Application/DataTransferObjects/ClientIdDTO";
 
 export default class ReservationArchiveController {
     public async archiveCancelledReservation(request: Request, response: Response,
@@ -29,5 +32,20 @@ export default class ReservationArchiveController {
             response.status(400).send();
         }
         response.status(200).send();
+    }
+
+    public async getCancelledReservations(request: Request, response: Response,
+                                          service: GetCancelledReservationsService): Promise<void> {
+        const { query } = request;
+
+        if (!query.clientId) {
+            response.status(400).send();
+            return;
+        }
+
+        // TODO discuss: query.clientId returns string | string[] | ParsedQs | ParsedQs[]
+        const dto = new ClientIdDTO(query.clientId.toString());
+        const ret: CancelledReservationDTO[] = await service.run(dto);
+        response.status(200).send(ret);
     }
 }
