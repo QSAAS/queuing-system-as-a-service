9c3b7ea9dc6240bce4f28c17732ec74633ac5533
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-02-13 14:46:21 +0200

    Implemented loading config based on set environment variable

--- src/SharedKernel/Services/ConfigReader.ts
+++ src/SharedKernel/Services/ConfigReader.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 import ConfigReadingError from "@app/SharedKernel/Errors/ConfigReadingError";
+import path from "path";
 
 export default class ConfigReader {
     private static isInitialized: boolean = false;
 
     public static read(key: string): string {
         if (!ConfigReader.isInitialized) {
-            dotenv.config();
-            this.isInitialized = true;
+            ConfigReader.initialize();
         }
 
         const value = process.env[key];
@@ -15,5 +15,24 @@ export default class ConfigReader {
             throw new ConfigReadingError(key);
         }
         return value;
+    }
+
+    /**
+     * loads environment variables defined in the configuration file based
+     * on the work environment
+     *
+     * ".env" and ".test.env" files must both be in the root directory of the project
+     * set the environment variable 'QSAS_ENV' to 'dev' to load from ".env"
+     * @private
+     */
+    private static initialize() {
+        const environment = process.env.QSAS_ENV;
+
+        if (environment === "dev") { // only read ".env" if environment is "dev", otherwise use ".test.env"
+            dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+        } else {
+            dotenv.config({ path: path.resolve(process.cwd(), ".test.env") });
+        }
+        this.isInitialized = true;
     }
 }
