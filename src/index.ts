1f1ea377d499f8b1acb7c96ce65c948437c69682
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-04-05 17:31:00 +0200

    [DevOps-2] edit index.ts to test db connection, and test entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 const port = process.env.SERVER_PORT || 8080;
+app.get("/", (request, response) => {
+    response.send("hello world");
+});
+app.get("/database-health", (request, response) => {
+    const dbUrl: string | undefined = process.env.DB_URL;
+    const { DB_PORT } = process.env;
+    const { DB_NAME } = process.env;
+    const connectionString = `mongodb://${dbUrl}:${DB_PORT}/${DB_NAME}`;
+    if (dbUrl) {
+        mongoose.connect(connectionString, { useUnifiedTopology: true, useNewUrlParser: true })
+            .then(() => {
+                console.log(`connected to ${dbUrl} successfully`);
+                response.send(`connected to ${dbUrl} successfully`);
+            }).catch((e) => {
+                console.log(`we faced an error in connecting to db ${e}`);
+            });
+    } else {
+        // fixme handle the case of failure in env variables
+
+    }
+});
 app.listen(port, () => {
     console.log(`Server started, listening on ${port}`);
 });
