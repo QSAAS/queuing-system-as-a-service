4c7200c7fefea32115b17153c2415b6e8318498d
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-04-29 23:15:06 +0200

    Changes to QueueNode members
    1- Added TimeSpan
    2- Removed readonly from endpointId and metaSpecs

--- Organization-Administration/src/Command/Domain/Entity/QueueNode.ts
+++ Organization-Administration/src/Command/Domain/Entity/QueueNode.ts
@@ -2,11 +2,13 @@ import AggregateRoot from "@app/Command/Domain/Entity/AggregateRoot";
 import QueueNodeId from "@app/Command/Domain/ValueObject/QueueNodeId";
 import OrganizationEndpointId from "@app/Command/Domain/ValueObject/OrganizationEndpointId";
 import MetadataSpecification from "@app/Command/Domain/ValueObject/MetadataSpecification";
+import TimeSpan from "@app/Command/Domain/ValueObject/TimeSpan";
 
 export default class QueueNode extends AggregateRoot {
-  constructor(private readonly id: QueueNodeId,
-    private readonly endpointId: OrganizationEndpointId,
-    private readonly metaSpecs: MetadataSpecification) {
+  constructor(public readonly id: QueueNodeId,
+    public readonly endpointId: OrganizationEndpointId,
+    public metaSpecs: MetadataSpecification,
+    public timeSpan: TimeSpan) {
     super();
   }
 }
