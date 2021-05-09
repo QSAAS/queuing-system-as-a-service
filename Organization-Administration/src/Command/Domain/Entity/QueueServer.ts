de27153ca5e8d13ad95b8b60904ee2a3ad44f24a
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-09 18:09:06 +0200

    [OA-H-1] init for QueueServer.ts, fix QueueServerId.ts

---
+++ Organization-Administration/src/Command/Domain/Entity/QueueServer.ts
@@ -0,0 +1,16 @@
+import AggregateRoot from "@app/Command/Domain/Entity/AggregateRoot";
+import QueueNodeId from "@app/Command/Domain/ValueObject/QueueNodeId";
+import QueueServerId from "@app/Command/Domain/ValueObject/QueueServerId";
+import OrganizationEndpointId from "@app/Command/Domain/ValueObject/OrganizationEndpointId";
+
+export default class QueueServer extends AggregateRoot {
+  protected queueServerId: QueueServerId;
+  protected organizationSEndpointId: OrganizationEndpointId;
+  protected serves : QueueNodeId;
+  constructor(queueServerId: QueueServerId, organizationSEndpointId: OrganizationEndpointId, serves : QueueNodeId) {
+    super();
+    this.queueServerId = queueServerId;
+    this.organizationSEndpointId = organizationSEndpointId;
+    this.serves = serves;
+  }
+}
