12f65e4b91870b1623a8efd5ba691967b8cb6ae3
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-09 21:32:45 +0200

    [OA-H-1] fix AdminstratedOrganizationEmployee.ts to use Super new constr, extend OrgE

--- Organization-Administration/src/Command/Domain/Entity/AdminstratedOrganizationEmployee.ts
+++ Organization-Administration/src/Command/Domain/Entity/AdminstratedOrganizationEmployee.ts
@@ -1,16 +1,17 @@
 import OrganizationEmployee from "@app/Command/Domain/Entity/OrganizationEmployee";
 import OrganizationEndpointAuthorizationService
   from "@app/Command/Application/Service/OrganizationEndpointAuthorizationService";
+import OrganizationEmployeeAuthorizationService
+  from "@app/Command/Application/Service/OrganizationEmployeeAuthorizationService";
 
-export default class AdminstratedOrganizationEmployee  extends OrganizationEmployee{
+export default class AdminstratedOrganizationEmployee extends OrganizationEmployee {
   private admin :OrganizationEmployee;
   private organizationEmployeeAuthorizationService : OrganizationEndpointAuthorizationService;
-  ///todo fix
+  /// todo fix
   constructor(admin :OrganizationEmployee, administrated: OrganizationEmployee,
-    organizationEmployeeAuthorizationService :OrganizationEndpointAuthorizationService) {
-    super(administrated.getOrganizationEmployeeId(),
-    administrated.getOrganizationId(), administrated.getName(),
-    administrated.getPasswordHash(), administrated.getUsername());
+    organizationEmployeeAuthorizationService :OrganizationEmployeeAuthorizationService) {
+    super(administrated.getOrganizationEmployeeId(), administrated.getOrganizationId(),
+      administrated.getName(), administrated.getPasswordHash(), administrated.getUsername());
     this.admin = admin;
     // this.administrated = administrated;
     this.organizationEmployeeAuthorizationService = organizationEmployeeAuthorizationService;
