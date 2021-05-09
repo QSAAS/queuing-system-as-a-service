52e538e45dc33699a425b2802f715ab19d7ea6d4
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-09 18:07:10 +0200

    [OA-H-1] fix AdminstratedOrganizationEmployee.ts

--- Organization-Administration/src/Command/Domain/Entity/AdminstratedOrganizationEmployee.ts
+++ Organization-Administration/src/Command/Domain/Entity/AdminstratedOrganizationEmployee.ts
@@ -2,14 +2,17 @@ import OrganizationEmployee from "@app/Command/Domain/Entity/OrganizationEmploye
 import OrganizationEndpointAuthorizationService
   from "@app/Command/Application/Service/OrganizationEndpointAuthorizationService";
 
-export default class AdminstratedOrganizationEmployee {
+export default class AdminstratedOrganizationEmployee  extends OrganizationEmployee{
   private admin :OrganizationEmployee;
-  private administrated :OrganizationEmployee;
   private organizationEmployeeAuthorizationService : OrganizationEndpointAuthorizationService;
+  ///todo fix
   constructor(admin :OrganizationEmployee, administrated: OrganizationEmployee,
     organizationEmployeeAuthorizationService :OrganizationEndpointAuthorizationService) {
+    super(administrated.getOrganizationEmployeeId(),
+    administrated.getOrganizationId(), administrated.getName(),
+    administrated.getPasswordHash(), administrated.getUsername());
     this.admin = admin;
-    this.administrated = administrated;
+    // this.administrated = administrated;
     this.organizationEmployeeAuthorizationService = organizationEmployeeAuthorizationService;
   }
   /// fixme why we need setters while we have them in OrganizationEmployee, should we aggregate it ?
