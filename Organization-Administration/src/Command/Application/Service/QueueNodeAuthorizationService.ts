7d4a09c2ac8432d886b8bb7168f31ac821b1f6e9
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-10 14:23:36 +0200

    [OA-H-1] add throws EmployeeNotAuthorizedError to signature

--- Organization-Administration/src/Command/Application/Service/QueueNodeAuthorizationService.ts
+++ Organization-Administration/src/Command/Application/Service/QueueNodeAuthorizationService.ts
@@ -2,7 +2,19 @@ import OrganizationId from "@app/Command/Domain/ValueObject/OrganizationId";
 import QueueNodeId from "@app/Command/Domain/ValueObject/QueueNodeId";
 
 export default interface QueueNodeAuthorizationService {
+  /**
+ *  @throws EmployeeNotAuthorizedError
+ *
+ * */
   ensureEmployeeCanCreate(organizationEmployeeId: OrganizationId): void;
+  /**
+   *  @throws EmployeeNotAuthorizedError
+   *
+   * */
   ensureEmployeeCanDelete(organizationEmployeeId: OrganizationId, queueNodeId: QueueNodeId): void;
+  /**
+   *  @throws EmployeeNotAuthorizedError
+   *
+   * */
   ensureEmployeeCanUpdate(organizationEmployeeId: OrganizationId, queueNodeId: QueueNodeId): void;
 }
--- Organization-Administration/src/Command/Application/Service/QueueServerAuthorizationService.ts
+++ Organization-Administration/src/Command/Application/Service/QueueServerAuthorizationService.ts
@@ -2,7 +2,21 @@ import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/Organization
 import QueueServerId from "@app/Command/Domain/ValueObject/QueueServerId";
 
 export default interface QueueServerAuthorizationService{
-  ensureEmployeeCanCreate(organizationEmployeeId: OrganizationEmployeeId):void ;
+  /**
+   * @throws EmployeeNotAuthorizedError
+   * @param organizationEmployeeId
+   * @param queueServerId
+   */
   ensureEmployeeCanDelete(organizationEmployeeId:OrganizationEmployeeId, queueServerId:QueueServerId):void ;
+  /**
+   * @throws EmployeeNotAuthorizedError
+   * @param organizationEmployeeId
+   * @param queueServerId
+   */
   ensureEmployeeCanUpdate(organizationEmployeeId:OrganizationEmployeeId, queueServerId:QueueServerId):void ;
+  /**
+   * @throws EmployeeNotAuthorizedError
+   * @param organizationEmployeeId
+   */
+  ensureEmployeeCanCreate(organizationEmployeeId: OrganizationEmployeeId):void ;
 }
