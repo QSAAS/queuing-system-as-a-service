5da6dcaab9ce4a77ded79dfc5e904204b59a4037
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-10 14:09:44 +0200

    [OA-H-1] add validation, throwing exception

--- Organization-Administration/src/Command/Domain/ValueObject/EmployeeUsername.ts
+++ Organization-Administration/src/Command/Domain/ValueObject/EmployeeUsername.ts
@@ -1,11 +1,21 @@
+import InvalidEmployeeUsernameError from "@app/Command/Domain/Error/InvalidEmployeeUsernameError";
+
 export default class EmployeeUsername {
   private readonly username: string ;
   constructor(username: string) {
-    this.username = username;
+    if (this.valid(username)) {
+      this.username = username;
+    } else {
+      throw new InvalidEmployeeUsernameError();
+    }
   }
 
   public toString():string {
     return this.username;
+  }
+
+  public valid(username: string):boolean {
+    return username.length > 4 && username.length < 50;
   }
   // todo how this supposed to throw an error exception
 }
