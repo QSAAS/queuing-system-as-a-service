d3651ec8bbc473fcaff3696c81a83b2c922910f6
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-10 12:53:13 +0200

    [OA-H-1] fix make att. protected,move setters to child

--- Organization-Administration/src/Command/Domain/Entity/AdministratedOrganizationEmployee.ts
+++ Organization-Administration/src/Command/Domain/Entity/AdministratedOrganizationEmployee.ts
@@ -3,11 +3,15 @@ import OrganizationEndpointAuthorizationService
   from "@app/Command/Application/Service/OrganizationEndpointAuthorizationService";
 import OrganizationEmployeeAuthorizationService
   from "@app/Command/Application/Service/OrganizationEmployeeAuthorizationService";
+import EmployeeUsername from "@app/Command/Domain/ValueObject/EmployeeUsername";
+import PasswordHash from "@app/Command/Domain/ValueObject/PasswordHash";
+import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
+import OrganizationId from "@app/Command/Domain/ValueObject/OrganizationId";
 
-export default class AdminstratedOrganizationEmployee extends OrganizationEmployee {
+export default class AdministratedOrganizationEmployee extends OrganizationEmployee {
   private admin :OrganizationEmployee;
   private organizationEmployeeAuthorizationService : OrganizationEndpointAuthorizationService;
-  /// todo fix
+
   constructor(admin :OrganizationEmployee, administrated: OrganizationEmployee,
     organizationEmployeeAuthorizationService :OrganizationEmployeeAuthorizationService) {
     super(administrated.getOrganizationEmployeeId(), administrated.getOrganizationId(),
@@ -16,6 +20,28 @@ export default class AdminstratedOrganizationEmployee extends OrganizationEmploy
     // this.administrated = administrated;
     this.organizationEmployeeAuthorizationService = organizationEmployeeAuthorizationService;
   }
+
+  public setUsername(username: EmployeeUsername) {
+    this.username = username;
+  }
+
+  public setName(name:string) {
+    this.name = name;
+  }
+
+  public setPasswordHash(passwordHash:PasswordHash) {
+    this.passwordHash = passwordHash;
+  }
+
+  public setOrganizationEmployeeId(organizationEmployeeId:OrganizationEmployeeId) {
+    this.organizationEmployeeId = organizationEmployeeId;
+  }
+
+  public setOrganizationId(organizationId:OrganizationId){
+    this.organizationId = organizationId;
+  }
+
   /// fixme why we need setters while we have them in OrganizationEmployee, should we aggregate it ?
   /// todo when we raise the event, and who raise it ?
 }
+/// todo remove setters from super, add them here, raise event with every set
