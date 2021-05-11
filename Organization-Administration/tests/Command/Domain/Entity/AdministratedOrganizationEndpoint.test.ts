46af63456e4bba1ea415a6336472cc6c66c686f0
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-05-11 13:39:46 +0200

    Modify AdministratedOrganizationEndpoint tests to use builder and mother

--- Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEndpoint.test.ts
+++ Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEndpoint.test.ts
@@ -1,80 +1,55 @@
-import OrganizationEndpointAuthorizationService
-  from "@app/Command/Application/Service/OrganizationEndpointAuthorizationService";
-import OrganizationEmployee from "@app/Command/Domain/Entity/OrganizationEmployee";
-import AdministratedOrganizationEndpoint from "@app/Command/Domain/Entity/AdministratedOrganizationEndpoint";
-import OrganizationEndpoint from "@app/Command/Domain/Entity/OrganizationEndpoint";
-import OrganizationId from "@app/Command/Domain/ValueObject/OrganizationId";
-import OrganizationEndpointId from "@app/Command/Domain/ValueObject/OrganizationEndpointId";
-import Geolocation from "@app/Command/Domain/ValueObject/Geolocation";
 import OrganizationEndpointUpdated from "@app/Command/Domain/Event/OrganizationEndpointUpdated";
-
-class MockOrganizationEndpointAuthorizationService implements OrganizationEndpointAuthorizationService {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  ensureCanEdit = jest.fn((organizationEmployee: OrganizationEmployee,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    administratedOrganizationEndpoint: AdministratedOrganizationEndpoint) => {
-  });
-}
+import AdministratedOrganizationEndpointMother
+  from "@tests/Command/Domain/Entity/AdministratedOrganizationEndpointMother";
+import GeolocationMother from "@tests/Command/Domain/ValueObject/GeolocationMother";
 
 describe("AdministratedOrganizationEndpoint", () => {
   describe("Can update name", () => {
-    const administratedOrganizationEndpoint = new AdministratedOrganizationEndpoint(new OrganizationEmployee(),
-      new OrganizationEndpoint(new OrganizationEndpointId("::id::"),
-        new OrganizationId("::id::"),
-        "::name::",
-        new Geolocation(0, 0)),
-      new MockOrganizationEndpointAuthorizationService());
-
     it("Updates name", () => {
+      const administratedOrganizationEndpointBuilder = AdministratedOrganizationEndpointMother.withPassingAuth();
+      const administratedOrganizationEndpoint = administratedOrganizationEndpointBuilder.build();
       administratedOrganizationEndpoint.setName("::name::");
       expect(administratedOrganizationEndpoint.getName()).toEqual("::name::");
     });
 
     it("Raises update event", () => {
-      const mockRaiseEvent = jest.fn();
-      administratedOrganizationEndpoint.raiseEvent = mockRaiseEvent;
+      const administratedOrganizationEndpointBuilder = AdministratedOrganizationEndpointMother.withPassingAuth();
+      const administratedOrganizationEndpoint = administratedOrganizationEndpointBuilder.build();
       administratedOrganizationEndpoint.setName("::name::");
-      expect(administratedOrganizationEndpoint.raiseEvent).toBeCalled();
-      expect(mockRaiseEvent.mock.calls[0][0]).toBeInstanceOf(OrganizationEndpointUpdated);
+      expect(administratedOrganizationEndpoint.getRaisedEvents()).toHaveLength(1);
+      expect(administratedOrganizationEndpoint.getRaisedEvents()[0]).toBeInstanceOf(OrganizationEndpointUpdated);
     });
 
-    it("Authorizes the administrator", () => {
-      administratedOrganizationEndpoint.setName("::name::");
-      expect(administratedOrganizationEndpoint
-        .getOrganizationEndpointAuthorizationService()
-        .ensureCanEdit)
-        .toBeCalled();
+    it("Rejects unauthorized administrator", () => {
+      const administratedOrganizationEndpointBuilder = AdministratedOrganizationEndpointMother.withFailingAuth();
+      const administratedOrganizationEndpoint = administratedOrganizationEndpointBuilder.build();
+      expect(() => { administratedOrganizationEndpoint.setName("::name::"); }).toThrow();
     });
   });
 
   describe("Can update geolocation", () => {
-    const administratedOrganizationEndpoint = new AdministratedOrganizationEndpoint(new OrganizationEmployee(),
-      new OrganizationEndpoint(new OrganizationEndpointId("::id::"),
-        new OrganizationId("::id::"),
-        "::name::",
-        new Geolocation(0, 0)),
-      new MockOrganizationEndpointAuthorizationService());
-    const geolocation = new Geolocation(1, 1);
-
     it("Updates geolocation", () => {
+      const administratedOrganizationEndpointBuilder = AdministratedOrganizationEndpointMother.withPassingAuth();
+      const administratedOrganizationEndpoint = administratedOrganizationEndpointBuilder.build();
+      const geolocation = GeolocationMother.complete().build();
       administratedOrganizationEndpoint.setGeolocation(geolocation);
       expect(administratedOrganizationEndpoint.getGeolocation()).toEqual(geolocation);
     });
 
     it("Raises update event", () => {
-      const mockRaiseEvent = jest.fn();
-      administratedOrganizationEndpoint.raiseEvent = mockRaiseEvent;
+      const administratedOrganizationEndpointBuilder = AdministratedOrganizationEndpointMother.withPassingAuth();
+      const administratedOrganizationEndpoint = administratedOrganizationEndpointBuilder.build();
+      const geolocation = GeolocationMother.complete().build();
       administratedOrganizationEndpoint.setGeolocation(geolocation);
-      expect(mockRaiseEvent).toBeCalled();
-      expect(mockRaiseEvent.mock.calls[0][0]).toBeInstanceOf(OrganizationEndpointUpdated);
+      expect(administratedOrganizationEndpoint.getRaisedEvents()).toHaveLength(1);
+      expect(administratedOrganizationEndpoint.getRaisedEvents()[0]).toBeInstanceOf(OrganizationEndpointUpdated);
     });
 
-    it("Authorizes the administrator", () => {
-      administratedOrganizationEndpoint.setGeolocation(geolocation);
-      expect(administratedOrganizationEndpoint
-        .getOrganizationEndpointAuthorizationService()
-        .ensureCanEdit)
-        .toBeCalled();
+    it("Rejects unauthorized administrator", () => {
+      const administratedOrganizationEndpointBuilder = AdministratedOrganizationEndpointMother.withFailingAuth();
+      const administratedOrganizationEndpoint = administratedOrganizationEndpointBuilder.build();
+      const geolocation = GeolocationMother.complete().build();
+      expect(() => { administratedOrganizationEndpoint.setGeolocation(geolocation); }).toThrow();
     });
   });
 });
