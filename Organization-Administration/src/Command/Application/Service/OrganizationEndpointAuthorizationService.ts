3188518e3f6ff1a14514b4bae70e0e161018f3f7
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-05-11 10:25:25 +0200

    Make auth service interface and add ensureCanEdit method

--- Organization-Administration/src/Command/Application/Service/OrganizationEndpointAuthorizationService.ts
+++ Organization-Administration/src/Command/Application/Service/OrganizationEndpointAuthorizationService.ts
@@ -1,1 +1,7 @@
-export default class OrganizationEndpointAuthorizationService {}
+import OrganizationEmployee from "@app/Command/Domain/Entity/OrganizationEmployee";
+import AdministratedOrganizationEndpoint from "@app/Command/Domain/Entity/AdministratedOrganizationEndpoint";
+
+export default interface OrganizationEndpointAuthorizationService {
+  ensureCanEdit(organizationEmployee: OrganizationEmployee,
+    AdministratedOrganizationEndpoint: AdministratedOrganizationEndpoint): void;
+}
