a2bc4ebd055b0bf865c749f58eb935fc0b0a11cb
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-05-11 13:34:20 +0200

    Add geolocation builder and mother

---
+++ Organization-Administration/tests/Command/Domain/ValueObject/GeolocationBuilder.ts
@@ -0,0 +1,20 @@
+import Geolocation from "@app/Command/Domain/ValueObject/Geolocation";
+
+export default class GeolocationBuilder {
+  private latitude: number = 0;
+  private longitude: number = 0;
+
+  public withLatitude(latitude: number): GeolocationBuilder {
+    this.latitude = latitude;
+    return this;
+  }
+
+  public withLongitude(longitude: number): GeolocationBuilder {
+    this.longitude = longitude;
+    return this;
+  }
+
+  public build(): Geolocation {
+    return Geolocation.create(this.latitude, this.longitude);
+  }
+}
