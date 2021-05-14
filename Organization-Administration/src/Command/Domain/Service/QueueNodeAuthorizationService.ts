99c806bb1581b489a06343530389297e619733ec
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-05-15 00:38:09 +0200

    Added throws annotation to QueueNodeAuthorizationService

--- Organization-Administration/src/Command/Domain/Service/QueueNodeAuthorizationService.ts
+++ Organization-Administration/src/Command/Domain/Service/QueueNodeAuthorizationService.ts
@@ -1,11 +1,12 @@
-// TODO move logic to some middleware?
 import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
 import QueueNodeId from "@app/Command/Domain/ValueObject/QueueNodeId";
 
 export default interface QueueNodeAuthorizationService {
+  /**
+   * @throws
+   * @param employeeId
+   */
   ensureEmployeeCanCreate(employeeId: OrganizationEmployeeId): void
   ensureEmployeeCanDelete(employeeId: OrganizationEmployeeId, queueNodeId: QueueNodeId): void
   ensureEmployeeCanUpdate(employeeId: OrganizationEmployeeId, queueNodeId: QueueNodeId): void
-
-  // TODO in the diagram, this throws an Error however this is an interface
 }
