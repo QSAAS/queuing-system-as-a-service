26c7df7a047e9b72dbb9f8175373818e8066fe0f
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-05-15 00:39:19 +0200

    Added setters

--- Organization-Administration/src/Command/Domain/ValueObject/MetadataSpecificationField.ts
+++ Organization-Administration/src/Command/Domain/ValueObject/MetadataSpecificationField.ts
@@ -1,7 +1,7 @@
 import ValueObject from "@app/Command/Domain/ValueObject/ValueObject";
 
-export default class MetadataSpecificationField extends ValueObject {
-  constructor(private name: string, private _isRequired: boolean) {
+export default abstract class MetadataSpecificationField extends ValueObject {
+  protected constructor(protected name: string, protected isRequired: boolean) {
     super();
   }
 
@@ -9,16 +9,15 @@ export default class MetadataSpecificationField extends ValueObject {
     return this.name;
   }
 
-  get isRequired(): boolean {
-    // eslint-disable-next-line no-underscore-dangle
-    return this._isRequired;
+  getIsRequired(): boolean {
+    return this.isRequired;
   }
 
-  // TODO why is this a class not an interface or an abstract class? do we need instances of this class?
-  // Classes that inherit this class (e.g. MetaDataSpecificationTextField) inherit this equals function,
-  // therefore they are not forced to implement it
-  // if someone forgot to implement equals() for subclasses, there will be a problem
-  equals(other: this): boolean {
-    throw new Error("Method not implemented");
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setIsRequired(isRequired: boolean) {
+    this.isRequired = isRequired;
   }
 }
