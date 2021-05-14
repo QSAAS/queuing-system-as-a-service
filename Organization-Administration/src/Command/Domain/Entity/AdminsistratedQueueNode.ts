b24338f941cc6d322f047b01b136296380b28813
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-05-15 00:36:42 +0200

    Added authentication service checking

--- Organization-Administration/src/Command/Domain/Entity/AdminsistratedQueueNode.ts
+++ Organization-Administration/src/Command/Domain/Entity/AdminsistratedQueueNode.ts
@@ -3,9 +3,12 @@ import TimeSpan from "@app/Command/Domain/ValueObject/TimeSpan";
 import MetadataSpecification from "@app/Command/Domain/ValueObject/MetadataSpecification";
 import QueueNodeUpdated from "@app/Command/Domain/Event/QueueNodeUpdated";
 import QueueNodeAuthorizationService from "@app/Command/Domain/Service/QueueNodeAuthorizationService";
+import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
 
-// TODO remove dummy classes
-class OrganizationEmployee {}
+// TODO remove type after merging
+type OrganizationEmployee = {
+  id: OrganizationEmployeeId,
+};
 
 export default class AdministratedQueueNode extends QueueNode {
   constructor(public readonly admin: OrganizationEmployee, public readonly queueNode: QueueNode,
@@ -14,14 +17,14 @@ export default class AdministratedQueueNode extends QueueNode {
   }
 
   setOperatingTimes(span: TimeSpan): void {
+    this.authService.ensureEmployeeCanUpdate(this.admin.id, this.id);
     this.timeSpan = span;
-    // TODO discuss: is this the correct way of raising events?
     this.raiseEvent(new QueueNodeUpdated(this));
   }
 
   setMetaDataSpecification(metaSpecs: MetadataSpecification) {
+    this.authService.ensureEmployeeCanUpdate(this.admin.id, this.id);
     this.metaSpecs = metaSpecs;
-    // TODO discuss: is this the correct way of raising events?
     this.raiseEvent(new QueueNodeUpdated(this));
   }
 }
