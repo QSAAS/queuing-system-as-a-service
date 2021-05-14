d0166cc3dd312fed74f569ac34e7e81b7e14df15
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-05-15 00:39:02 +0200

    Implemented equals function

--- Organization-Administration/src/Command/Domain/ValueObject/MetadataSpecificationDropdownField.ts
+++ Organization-Administration/src/Command/Domain/ValueObject/MetadataSpecificationDropdownField.ts
@@ -8,4 +8,17 @@ export default class MetadataSpecificationDropdownField extends MetadataSpecific
   getOptions(): string[] {
     return this.options;
   }
+
+  equals(other: this): boolean {
+    return this.name === other.name
+        && this.isRequired === other.isRequired
+        && this.options.length === other.options.length
+        && this.options.every(
+          (element, index) => element === other.options[index],
+        );
+  }
+
+  setOptions(options: string[]) {
+    this.options = options;
+  }
 }
--- Organization-Administration/src/Command/Domain/ValueObject/MetadataSpecificationTextField.ts
+++ Organization-Administration/src/Command/Domain/ValueObject/MetadataSpecificationTextField.ts
@@ -1,8 +1,12 @@
 import MetadataSpecificationField from "@app/Command/Domain/ValueObject/MetadataSpecificationField";
 
 export default class MetadataSpecificationTextField extends MetadataSpecificationField {
-  constructor(name: string, isRequired: boolean, private maxLength: number, private minLength: number,
-    private regex: string, private placeholder: string) {
+  constructor(name: string,
+    isRequired: boolean,
+    private maxLength: number,
+    private minLength: number,
+    private regex: string, // TODO use RegExp?
+    private placeholder: string) {
     super(name, isRequired);
   }
 
@@ -10,15 +14,40 @@ export default class MetadataSpecificationTextField extends MetadataSpecificatio
     return this.maxLength;
   }
 
+  setMaxLength(value: number): void {
+    this.maxLength = value;
+  }
+
   getMinLength(): number {
     return this.minLength;
+  }
+
+  setMinLength(value: number): void {
+    this.minLength = value;
   }
 
   getRegex(): string {
     return this.regex;
   }
 
+  setRegex(value: string): void {
+    this.regex = value;
+  }
+
   getPlaceholder(): string {
     return this.placeholder;
+  }
+
+  setPlaceHolder(value: string): void {
+    this.placeholder = value;
+  }
+
+  equals(other: this): boolean {
+    return this.name === other.name
+        && this.isRequired === other.isRequired
+        && this.maxLength === other.maxLength
+        && this.minLength === other.minLength
+        && this.regex === other.regex
+        && this.placeholder === other.placeholder;
   }
 }
