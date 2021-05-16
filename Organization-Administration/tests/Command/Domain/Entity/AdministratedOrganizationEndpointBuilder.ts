3c6da5e8a12626ae885b831bd1cffba77d48eeca
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-05-16 21:01:39 +0200

    [HF-1] fix Mostafa's test to use OEmployee class

--- Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEndpointBuilder.ts
+++ Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEndpointBuilder.ts
@@ -3,9 +3,19 @@ import OrganizationEndpointAuthorizationService
   from "@app/Command/Application/Service/OrganizationEndpointAuthorizationService";
 import OrganizationEndpointBuilder from "@tests/Command/Domain/Entity/OrganizationEndpointBuilder";
 import AdministratedOrganizationEndpoint from "@app/Command/Domain/Entity/AdministratedOrganizationEndpoint";
+import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
+import OrganizationId from "@app/Command/Domain/ValueObject/OrganizationId";
+import PasswordHash from "@app/Command/Domain/ValueObject/PasswordHash";
+import TrialPaswordHash from "@tests/Command/Domain/ValueObject/TrialPaswordHash";
+import EmployeeUsername from "@app/Command/Domain/ValueObject/EmployeeUsername";
 
 export default class AdministratedOrganizationEndpointBuilder extends OrganizationEndpointBuilder {
-  private administrator: OrganizationEmployee = new OrganizationEmployee();
+  private adminEID: OrganizationEmployeeId = new OrganizationEmployeeId("123");
+  private adminOID: OrganizationId = new OrganizationId("1");
+  private adminPasswordHash : PasswordHash = new TrialPaswordHash("passwordHash");
+  private adminUsername : EmployeeUsername = new EmployeeUsername("adminusername");
+  // eslint-disable-next-line max-len
+  private administrator: OrganizationEmployee = new OrganizationEmployee(this.adminEID, this.adminOID, "admin", this.adminPasswordHash, this.adminUsername);
   private organizationEndpointAuthorizationService: OrganizationEndpointAuthorizationService | undefined;
 
   public withAdministrator(administrator: OrganizationEmployee): AdministratedOrganizationEndpointBuilder {
