d1b84294aaf5473e42f61a8ef07aca536b00cb0b
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-06-08 08:05:26 +0200

    Add builder defaults

--- Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEndpointBuilder.ts
+++ Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEndpointBuilder.ts
@@ -3,10 +3,15 @@ import OrganizationEndpointAuthorizationService
   from "@app/Command/Application/Service/OrganizationEndpointAuthorizationService";
 import OrganizationEndpointBuilder from "@tests/Command/Domain/Entity/OrganizationEndpointBuilder";
 import AdministratedOrganizationEndpoint from "@app/Command/Domain/Entity/AdministratedOrganizationEndpoint";
+import PassingOrganizationEndpointAuthorizationService
+  from "@tests/Command/Infrastructure/PassingOrganizationEndpointAuthorizationService";
 
 export default class AdministratedOrganizationEndpointBuilder extends OrganizationEndpointBuilder {
-  private administrator: OrganizationEmployee = new OrganizationEmployee();
-  private organizationEndpointAuthorizationService: OrganizationEndpointAuthorizationService | undefined;
+  constructor(private administrator: OrganizationEmployee = new OrganizationEmployee(),
+    private organizationEndpointAuthorizationService: OrganizationEndpointAuthorizationService
+    = new PassingOrganizationEndpointAuthorizationService()) {
+    super();
+  }
 
   public withAdministrator(administrator: OrganizationEmployee): AdministratedOrganizationEndpointBuilder {
     this.administrator = administrator;
@@ -21,10 +26,6 @@ export default class AdministratedOrganizationEndpointBuilder extends Organizati
   }
 
   public build(): AdministratedOrganizationEndpoint {
-    if (!this.organizationEndpointAuthorizationService) {
-      throw new TypeError("AdministratedOrganizationEndpoint can't be built with"
-        + "undefined OrganizationEndpointAuthorizationService");
-    }
     return new AdministratedOrganizationEndpoint(
       this.administrator,
       super.build(),
