dba08aa534ff883eb53204379b2e219441a13acb
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-06-10 14:57:54 +0200

    Add AuthorizationRuleAuthorizationService

---
+++ Organization-Administration/src/Command/Domain/Service/AuthorizationRuleAuthorizationService.ts
@@ -0,0 +1,5 @@
+import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
+
+export default interface AuthorizationRuleAuthorizationService{
+  ensureEmployeeCanEdit(employeeId: OrganizationEmployeeId): void
+}
