8d685b49e2e1681cdca1a39114facae171d4a633
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-06-13 06:23:49 +0200

    [OA-H-1] fix missing parameters in functions,used admin, administrated

--- Organization-Administration/src/Command/Application/Service/OrganizationEmployeeAuthorizationService.ts
+++ Organization-Administration/src/Command/Application/Service/OrganizationEmployeeAuthorizationService.ts
@@ -2,6 +2,6 @@ import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/Organization
 
 export default interface OrganizationEmployeeAuthorizationService {
   ensureEmployeeCanCreate(organizationEmployeeId: OrganizationEmployeeId):void ;
-  ensureEmployeeCanDelete(organizationEmployeeId:OrganizationEmployeeId):void ;
-  ensureEmployeeCanUpdate(organizationEmployeeId:OrganizationEmployeeId):void ;
+  ensureEmployeeCanDelete(admin:OrganizationEmployeeId, administrated: OrganizationEmployeeId):void ;
+  ensureEmployeeCanUpdate(admin:OrganizationEmployeeId, administrated: OrganizationEmployeeId):void ;
 }
