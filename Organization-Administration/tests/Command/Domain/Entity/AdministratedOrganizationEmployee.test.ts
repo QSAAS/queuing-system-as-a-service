e1a51f29766bad026e1676c72c3046a7c21a018c
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-06-13 09:19:43 +0200

    [OA-H-1] test exceptions in AdministratedOrganizationEmployee.test.ts

--- Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEmployee.test.ts
+++ Organization-Administration/tests/Command/Domain/Entity/AdministratedOrganizationEmployee.test.ts
@@ -1,6 +1,9 @@
 import AdministratedOrganizationEmployee from "@app/Command/Domain/Entity/AdministratedOrganizationEmployee";
 import AdministratedOrganizationEmployeeBuilder
   from "@tests/Command/Domain/Entity/AdministratedOrganizationEmployeeBuilder";
+import FailingOrganizationEmployeeAuthorizationService
+  from "@tests/Command/Infrastructure/FailingOrganizationEmployeeAuthorizationService";
+import EmployeeNotAuthorizedError from "@app/Command/Domain/Error/EmployeeNotAuthorizedError";
 
 describe("test AdministratedOrganization Employee", () => {
   describe("Events", () => {
@@ -19,11 +22,58 @@ describe("test AdministratedOrganization Employee", () => {
       expect(administratedEmployee.getRaisedEvents().length).toEqual(1);
     });
 
-    it("should raise event when set passwordHash", () => {
+    it("raise event when set passwordHash", () => {
       const administratedEmployee: AdministratedOrganizationEmployee = new
       AdministratedOrganizationEmployeeBuilder().build();
       administratedEmployee.setPasswordHash(administratedEmployee.getPasswordHash());
       expect(administratedEmployee.getRaisedEvents().length).toEqual(1);
+    });
+  });
+  describe("Exceptions", () => {
+    it("Throw exception when set passwordHash with failing OrgEmpAuthServ", () => {
+      const administratedEmployee: AdministratedOrganizationEmployee = new
+      AdministratedOrganizationEmployeeBuilder()
+        .withOEAuthService(new FailingOrganizationEmployeeAuthorizationService()).build();
+
+      expect(() => {
+        administratedEmployee.setPasswordHash(administratedEmployee.getPasswordHash());
+      }).toThrow(EmployeeNotAuthorizedError);
+    });
+    it("Throw exception when set Name with failing OrgEmpAuthServ", () => {
+      const administratedEmployee: AdministratedOrganizationEmployee = new
+      AdministratedOrganizationEmployeeBuilder()
+        .withOEAuthService(new FailingOrganizationEmployeeAuthorizationService()).build();
+
+      expect(() => {
+        administratedEmployee.setName(administratedEmployee.getName());
+      }).toThrow(EmployeeNotAuthorizedError);
+    });
+    it("Throw exception when set username with failing OrgEmpAuthServ", () => {
+      const administratedEmployee: AdministratedOrganizationEmployee = new
+      AdministratedOrganizationEmployeeBuilder()
+        .withOEAuthService(new FailingOrganizationEmployeeAuthorizationService()).build();
+
+      expect(() => {
+        administratedEmployee.setUsername(administratedEmployee.getUsername());
+      }).toThrow(EmployeeNotAuthorizedError);
+    });
+    it("Throw exception when set OrganizationEmployeeId with failing OrgEmpAuthServ", () => {
+      const administratedEmployee: AdministratedOrganizationEmployee = new
+      AdministratedOrganizationEmployeeBuilder()
+        .withOEAuthService(new FailingOrganizationEmployeeAuthorizationService()).build();
+
+      expect(() => {
+        administratedEmployee.setOrganizationEmployeeId(administratedEmployee.getOrganizationEmployeeId());
+      }).toThrow(EmployeeNotAuthorizedError);
+    });
+    it("Throw exception when set OrganizationId with failing OrgEmpAuthServ", () => {
+      const administratedEmployee: AdministratedOrganizationEmployee = new
+      AdministratedOrganizationEmployeeBuilder()
+        .withOEAuthService(new FailingOrganizationEmployeeAuthorizationService()).build();
+
+      expect(() => {
+        administratedEmployee.setOrganizationId(administratedEmployee.getOrganizationId());
+      }).toThrow(EmployeeNotAuthorizedError);
     });
   });
 });
