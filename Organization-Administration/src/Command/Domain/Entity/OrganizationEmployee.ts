ca57cd13dbde4b4bae13736a6eed8fde17a4787e
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-06-13 14:03:45 +0200

    [OA-H-1] fix constructor, remove create

--- Organization-Administration/src/Command/Domain/Entity/OrganizationEmployee.ts
+++ Organization-Administration/src/Command/Domain/Entity/OrganizationEmployee.ts
@@ -1,29 +1,21 @@
 import AggregateRoot from "@app/Command/Domain/Entity/AggregateRoot";
 import OrganizationId from "@app/Command/Domain/ValueObject/OrganizationId";
-import DomainEvent from "@app/Command/Domain/Event/DomainEvent";
 import PasswordHash from "@app/Command/Domain/ValueObject/PasswordHash";
 import EmployeeUsername from "@app/Command/Domain/ValueObject/EmployeeUsername";
 import OrganizationEmployeeId from "../ValueObject/OrganizationEmployeeId";
 
 export default class OrganizationEmployee extends AggregateRoot {
-  protected organizationEmployeeId: OrganizationEmployeeId;
-  protected organizationId: OrganizationId;
-  protected name: string;
-  protected passwordHash: PasswordHash;
-  protected username: EmployeeUsername;
-
-  constructor(organizationEmployeeId: OrganizationEmployeeId,
-    organizationId: OrganizationId, name: string, passwordHash: PasswordHash, username: EmployeeUsername) {
+  constructor(private readonly organizationEmployeeId: OrganizationEmployeeId,
+    private readonly organizationId: OrganizationId,
+    private readonly name: string,
+    private readonly passwordHash: PasswordHash,
+    private readonly username: EmployeeUsername) {
     super();
     this.name = name;
     this.organizationEmployeeId = organizationEmployeeId;
     this.organizationId = organizationId;
     this.passwordHash = passwordHash;
     this.username = username;
-  }
-
-  getRaisedEvents(): DomainEvent[] {
-    return super.getRaisedEvents();
   }
 
   public getOrganizationEmployeeId(): OrganizationEmployeeId {
@@ -46,14 +38,7 @@ export default class OrganizationEmployee extends AggregateRoot {
     return this.username;
   }
 
-  public static create(organizationEmployeeId: OrganizationEmployeeId,
-    organizationId: OrganizationId, name: string,
-    passwordHash: PasswordHash, username: EmployeeUsername): OrganizationEmployee {
-    return new OrganizationEmployee(organizationEmployeeId, organizationId, name, passwordHash, username);
-  }
-
   public getId(): OrganizationEmployeeId {
-    // dummy value until class implementation is provided
     return this.organizationEmployeeId;
   }
 }
