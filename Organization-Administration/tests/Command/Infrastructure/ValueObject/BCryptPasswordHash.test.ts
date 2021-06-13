5f601698a3c9b3a3a3feab71aa76eb8c3883c107
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2021-06-13 23:36:20 +0200

    Exit docker compose on completion

--- Organization-Administration/tests/Command/Infrastructure/ValueObject/BCryptPasswordHash.test.ts
+++ Organization-Administration/tests/Command/Infrastructure/ValueObject/BCryptPasswordHash.test.ts
@@ -7,7 +7,7 @@ describe("Can match passwords", () => {
     expect(await passwordHash.matches("1234")).toBe(false);
   });
   it("Returns true on matching passwords", async () => {
-    const passwordHash = await factory.create("123412345");
+    const passwordHash = await factory.create("123412344");
     expect(await passwordHash.matches("12341234")).toBe(true);
   });
 });
