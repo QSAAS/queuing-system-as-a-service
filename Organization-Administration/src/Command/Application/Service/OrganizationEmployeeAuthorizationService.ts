5288c0c894da3513e4e33cbaecf87a998faf7f96
Ahmed Hanafy <ahmedhanfy1111@gmail.com>
2021-06-13 06:28:32 +0200

    [OA-H-1] add documentation for OrganizationEmployeeAuthorizationService.ts

--- Organization-Administration/src/Command/Application/Service/OrganizationEmployeeAuthorizationService.ts
+++ Organization-Administration/src/Command/Application/Service/OrganizationEmployeeAuthorizationService.ts
@@ -1,7 +1,21 @@
 import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
 
 export default interface OrganizationEmployeeAuthorizationService {
-  ensureEmployeeCanCreate(organizationEmployeeId: OrganizationEmployeeId):void ;
+  /**
+   * @throws EmployeeNotAuthorizedError
+   * @param admin
+   */
+  ensureEmployeeCanCreate(admin: OrganizationEmployeeId):void ;
+  /**
+   * @throws EmployeeNotAuthorizedError
+   * @param admin
+   * @param administrated
+   */
   ensureEmployeeCanDelete(admin:OrganizationEmployeeId, administrated: OrganizationEmployeeId):void ;
+  /**
+   * @throws EmployeeNotAuthorizedError
+   * @param admin
+   * @param administrated
+   */
   ensureEmployeeCanUpdate(admin:OrganizationEmployeeId, administrated: OrganizationEmployeeId):void ;
 }
