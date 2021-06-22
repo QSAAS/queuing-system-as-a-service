ba4c688e9d1df6c166299a8dcffec5acd2543244
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-06-22 16:57:40 +0200

    Removed function comments and added class comment

--- Organization-Administration/src/Command/Domain/Service/QueueNodeAuthorizationService.ts
+++ Organization-Administration/src/Command/Domain/Service/QueueNodeAuthorizationService.ts
@@ -1,24 +1,14 @@
 import OrganizationEmployeeId from "@app/Command/Domain/ValueObject/OrganizationEmployeeId";
 import QueueNodeId from "@app/Command/Domain/ValueObject/QueueNodeId";
 
+/**
+ * @throws EmployeeNotAuthorizedError
+ */
 export default interface QueueNodeAuthorizationService {
-  /**
-   * @throws EmployeeNotAuthorizedError
-   * @param employeeId
-   */
+
   ensureEmployeeCanCreate(employeeId: OrganizationEmployeeId): Promise<void>;
 
-  /**
-   * @throws EmployeeNotAuthorizedError
-   * @param employeeId
-   * @param queueNodeId
-   */
   ensureEmployeeCanDelete(employeeId: OrganizationEmployeeId, queueNodeId: QueueNodeId): Promise<void>;
 
-  /**
-   * @throws EmployeeNotAuthorizedError
-   * @param employeeId
-   * @param queueNodeId
-   */
   ensureEmployeeCanUpdate(employeeId: OrganizationEmployeeId, queueNodeId: QueueNodeId): Promise<void>;
 }
