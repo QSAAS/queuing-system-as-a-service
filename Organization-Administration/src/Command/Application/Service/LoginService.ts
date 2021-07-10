e68d271de8499286aa8720e0f4282aee978711c1
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-07-10 15:55:09 +0200

    fix issue where LoginService doesn't throw if username doesn't exist

--- Organization-Administration/src/Command/Application/Service/LoginService.ts
+++ Organization-Administration/src/Command/Application/Service/LoginService.ts
@@ -2,6 +2,8 @@ import OrganizationEmployeeRepository from "@app/Command/Domain/Service/Organiza
 import LoginRequest from "@app/Command/Application/DataTransferObject/Request/LoginRequest";
 import EmployeeUsername from "@app/Command/Domain/ValueObject/EmployeeUsername";
 import IncorrectCredentialsError from "@app/Command/Application/Error/IncorrectCredentialsError";
+import OrganizationEmployee from "@app/Command/Domain/Entity/OrganizationEmployee";
+import OrganizationEmployeeNotFound from "@app/Command/Domain/Error/OrganizationEmployeeNotFound";
 
 export default class LoginService {
   constructor(
@@ -9,11 +11,23 @@ export default class LoginService {
   ){}
 
   async execute(request: LoginRequest): Promise<string> {
-    const employee = await this.employeeRepo.getByUsername(EmployeeUsername.from(request.username));
+    const errorMessage = "Incorrect username or password.";
+
+    let employee: OrganizationEmployee;
+    try {
+      employee = await this.employeeRepo.getByUsername(EmployeeUsername.from(request.username));
+    } catch (e) {
+      if (e instanceof OrganizationEmployeeNotFound) {
+        throw new IncorrectCredentialsError(errorMessage);
+      }
+      throw e;
+    }
+
     const isCorrectPassword: boolean = await employee.getPasswordHash().matches(request.password);
     if (!isCorrectPassword) {
-      throw new IncorrectCredentialsError("Incorrect username or password.");
+      throw new IncorrectCredentialsError(errorMessage);
     }
+
     return employee.getId().toString();
   }
 }
