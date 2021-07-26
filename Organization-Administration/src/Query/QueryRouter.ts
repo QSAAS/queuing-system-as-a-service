69be0399f4aca5ea3272cb8f785469e5fd57fa27
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-07-26 06:06:27 +0200

    Add Queue node query

--- Organization-Administration/src/Query/QueryRouter.ts
+++ Organization-Administration/src/Query/QueryRouter.ts
@@ -4,13 +4,13 @@ import DependencyInjectionContainer from "@app/Command/Infrastructure/Config/Dep
 import {DiEntry} from "@app/Command/Infrastructure/Config/DependencyDefinitions";
 import createOrganizationEndpointQueryRouter from "@app/Query/OrganizationEndpointQueryRouter";
 import createOrganizationEmployeeQueryRouter from "@app/Query/OrganizationEmployeeQueryRouter";
+import createQueueNodeQueryRouter from "@app/Query/QueueNodeQueryRouter";
 
 export default function createQueryRouter(container: DependencyInjectionContainer<DiEntry>) {
   const router = express.Router();
   router.use("/endpoint", createOrganizationEndpointQueryRouter(container));
   router.use("/accounts", createOrganizationEmployeeQueryRouter(container));
-  // router.use("/authorization-rule", createAuthorizationRuleQueryRouter(container));
-  // router.use("/queue_node", createQueueNodeQueryRouter(container));
+  router.use("/queue_node", createQueueNodeQueryRouter(container));
   // router.use("/queue_server", createQueueServerQueryRouter(container));
   router.use(ErrorHandler);
 
